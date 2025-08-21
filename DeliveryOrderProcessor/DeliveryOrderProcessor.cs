daa5159e2ab53afa0c39b3074e0addca8d628482
Anastasiya Trayanava <anastasiya.trayanava@ensek.co.uk>
2025-08-21 20:04:16 +0200

    Add project files.

---
+++ DeliveryOrderProcessor/DeliveryOrderProcessor.cs
@@ -0,0 +1,58 @@
+using System.Net;
+using Azure.Identity;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Azure.Cosmos;
+using Microsoft.Azure.Functions.Worker;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+
+namespace DeliveryOrderProcessor;
+
+public class DeliveryOrderProcessor
+{
+    private readonly ILogger<DeliveryOrderProcessor> _logger;
+    private readonly Container _container;
+
+    public DeliveryOrderProcessor(ILogger<DeliveryOrderProcessor> logger)
+    {
+        _logger = logger;
+
+        CosmosClient client = new(
+	        accountEndpoint: Environment.GetEnvironmentVariable("ConnectionString"),
+	        tokenCredential: new DefaultAzureCredential()
+        );
+
+        var database = client.GetDatabase("things");
+        var container = database.GetContainer("deliveryorders");
+
+		_container = container;
+    }
+
+    [Function("DeliveryOrderProcessor")]
+    public async Task<IActionResult> Run([HttpTrigger(AuthorizationLevel.Function, "post")] HttpRequest req)
+    {
+        _logger.LogInformation("C# HTTP trigger function processed a request.");
+
+        var reader = new StreamReader(req.Body);
+        var response = await reader.ReadToEndAsync();
+
+        var orderDetailsObj = JsonConvert.DeserializeObject<OrderDetails>(response);
+
+        if (orderDetailsObj == null)
+        {
+	        return new BadRequestObjectResult("Incoming object is empty.");
+        }
+
+        orderDetailsObj.Id = Guid.NewGuid().ToString();
+
+        var dbResponse = await _container.CreateItemAsync<OrderDetails>(orderDetailsObj, PartitionKey.None);
+
+        if (dbResponse.StatusCode == HttpStatusCode.Accepted)
+        {
+			return new OkObjectResult("It is done!");
+        }
+
+        return new BadRequestResult();
+    }
+}
---
+++ DeliveryOrderProcessor/OrderDetails.cs
@@ -0,0 +1,26 @@
+﻿namespace DeliveryOrderProcessor
+{
+	public class OrderDetails
+	{
+		public string Id { get; set; }
+		public ShippingAddress ShippingAddress { get; set; }
+		public decimal FinalPrice { get; set; }
+		public List<Item> Items { get; set; }
+	}
+
+	public class Item
+	{
+		public int Count { get; set; }
+		public int ItemId { get; set; }
+		public string Name { get; set; }
+	}
+
+	public class ShippingAddress
+	{
+		public string Street { get; set; }
+		public string City { get; set; }
+		public string State { get; set; }
+		public string Country { get; set; }
+		public string ZipCode { get; set; }
+	}
+}
